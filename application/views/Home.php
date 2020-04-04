0fda16cc2f50d9f50bfdd4904e596a79a4e5387c
figoakbar <49114403+figoakbar@users.noreply.github.com>
2020-04-04 19:29:42 +0700

    Update Home.php

--- application/views/Home.php
+++ application/views/Home.php
@@ -113,7 +113,7 @@
 
                         <li class="nav-item">
                         <li class="nav-item">
-                            <a class="nav-link" href="/login.html">
+                            <a class="nav-link" href="<?php echo base_url('index.php/Home/Login/') ?>">
                                 Masuk
                             </a>
                         </li>
