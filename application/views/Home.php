789349ba36c855260c38048b82faf2b891dbf075
figoakbar <49114403+figoakbar@users.noreply.github.com>
2020-04-05 22:26:09 +0700

    Update Home.php

--- application/views/Home.php
+++ application/views/Home.php
@@ -78,31 +78,30 @@
     </style>
 </head>
 <body style="background-color:#f4f4f4">
-    <header>
-    <div class="navbar navbar-expand-md navbar-light mb-4 "  role="navigation">
-        <a class="navbar-brand" href="#">Logo</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: white;">
+        <a class="navbar-brand" href="#">LOGO</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#" target="_blank">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Register</a>
-                </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" style="color: #17A1EF;">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Daftar</a>
+            </li>
             </ul>
-            <form class="form-inline mt-2 mt-md-0">
-                <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+            <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" style="border-radius: 8px;">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit" style="border-radius: 8px;">Search</button>
             </form>
         </div>
-    </div>
-    </header>
+    </nav>
 
     <section id="carousel" style="">
         <div class="container">
