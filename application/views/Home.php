6ab3542fa9b63d55dcfdf27fc2bb3ec7bd79a89a
Aldy Renaldi <44864241+aldyrenaldi@users.noreply.github.com>
2020-04-05 22:38:41 +0700

    Update Home.php

--- application/views/Home.php
+++ application/views/Home.php
@@ -79,7 +79,7 @@
 </head>
 <body style="background-color:#f4f4f4">
     <nav class="navbar navbar-expand-lg navbar-light" style="background-color: white;">
-        <a class="navbar-brand" href="#">LOGO</a>
+        <a class="navbar-brand" href="#"><img src="assets/image/Logo.png" alt=""></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
