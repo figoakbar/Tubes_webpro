f4c1637bf8acd710785779842a60ce1be3f36c97
figoakbar <49114403+figoakbar@users.noreply.github.com>
2020-04-08 15:25:54 +0700

    Update Imunisasi.php

--- application/controllers/Imunisasi.php
+++ application/controllers/Imunisasi.php
@@ -13,6 +13,6 @@ class Imunisasi extends CI_Controller {
     public function index()
     {
         //Load page_header and page_index from views
-        $this->load->view('Home');
+        $this->load->view('pesanobat');
     }
 }
