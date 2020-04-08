e232aeee8401467525803c3f6d291d4a1bdb2cba
figoakbar <49114403+figoakbar@users.noreply.github.com>
2020-04-08 15:25:57 +0700

    Create pesanobat.php

---
+++ application/views/pesanobat.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href= "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+   </head>
+   <style>
+        .title{
+            background-image: url('assets/image/iStock-638377134.jpg');
+        }
+
+        .card{
+            box-shadow: 1px 1px 1px rgba(0,0,0,0.8);
+        }
+        .card-title{
+            color: #17A1EF;
+        }
+        
+   </style>
+   <body>
+    <!-- As a heading -->
+	<nav class="navbar navbar-expand-lg navbar-light" style="background-color: white;">
+        <a class="navbar-brand" href="#"><img src="assets/image/Logo.png" alt=""></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Imunisasi</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"  style="color: #17A1EF;">Pesan Obat</a>
+            </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" style="border-radius: 8px;">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit" style="border-radius: 8px;">Search</button>
+            </form>
+        </div>
+    </nav>
+	<div class="title" style="padding-bottom: 200px; margin: 0px;">
+	    <div class="container" style="margin-top: 0px;">
+            <h1 style="vertical-align: 30px;">LINDUNGI ANAK ANDA.</h1>
+	    </div>
+	</div>
+	<div style="background-color:white;">
+	    	<div class="container" style="padding-bottom: 80px;">
+            <div style="padding : 50px;">
+                <span><input class="form-control mr-sm-2" type="search" placeholder="Cari obat" aria-label="Search" style="border-radius: 8px; width : 70%; margin-left: 40px;">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit" style="border-radius: 8px;">Search</button></span>
+            </div>
+			  <div class="row">
+              <div class="col-lg-4">
+                <div class="card container" id="card" style="width: 18rem;border-radius: 30px;">
+                    <img class="card-img-top" src="https://ecs7.tokopedia.net/img/cache/700/product-1/2018/2/2/150923423/150923423_688a7d67-dda4-41bd-849b-b4194f67a04a_1000_1000.png" alt="Card image cap" id="card-gambar" style="border-radius: 30px;">
+                    <div class="card-body">
+                        <h5 class="card-title" id="nama-obat">Profis Ibuprofen</h5>
+                        <p>Lorem ipsum dolor sit amet..</p>
+                        <a href="#" class="btn btn-primary">Pesan</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card container" style="width: 18rem; border-radius: 30px;">
+                <img class="card-img-top" src="https://ecs7.tokopedia.net/img/cache/700/product-1/2018/2/2/150923423/150923423_688a7d67-dda4-41bd-849b-b4194f67a04a_1000_1000.png" alt="Card image cap" id="card-gambar" style="border-radius: 30px;">
+                    <div class="card-body">
+                        <h5 class="card-title" id="nama-obat">Profis Ibuprofen</h5>
+                        <p>Lorem ipsum dolor sit amet..</p>
+                        <a href="#" class="btn btn-primary">Pesan</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card container" style="width: 18rem; border-radius: 30px;">
+                <img class="card-img-top" src="https://ecs7.tokopedia.net/img/cache/700/product-1/2018/2/2/150923423/150923423_688a7d67-dda4-41bd-849b-b4194f67a04a_1000_1000.png" alt="Card image cap" id="card-gambar"style="border-radius: 30px;">
+                    <div class="card-body">
+                        <h5 class="card-title" id="nama-obat">Profis Ibuprofen</h5>
+                        <p>Lorem ipsum dolor sit amet..</p>
+                        <a href="#" class="btn btn-primary">Pesan</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+			 <div class="row" style="margin-top: 50px;">
+             <div class="col-lg-4">
+                <div class="card container " style="width: 18rem;border-radius: 30px;">
+                    <img class="card-img-top" src="https://ecs7.tokopedia.net/img/cache/700/product-1/2018/2/2/150923423/150923423_688a7d67-dda4-41bd-849b-b4194f67a04a_1000_1000.png" alt="Card image cap" id="card-gambar"style="border-radius: 30px;">
+                    <div class="card-body">
+                            <h5 class="card-title" id="nama-obat">Profis Ibuprofen</h5>
+                            <p>Lorem ipsum dolor sit amet..</p>
+                            <a href="#" class="btn btn-primary">Pesan</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card container" style="width: 18rem;border-radius: 30px;">
+                <img class="card-img-top" src="https://ecs7.tokopedia.net/img/cache/700/product-1/2018/2/2/150923423/150923423_688a7d67-dda4-41bd-849b-b4194f67a04a_1000_1000.png" alt="Card image cap" id="card-gambar"style="border-radius: 30px;">
+                    <div class="card-body">
+                        <h5 class="card-title" id="nama-obat">Profis Ibuprofen</h5>
+                        <p>Lorem ipsum dolor sit amet..</p>
+                        <a href="#" class="btn btn-primary">Pesan</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card container" style="width: 18rem;border-radius: 30px;">
+                    <img class="card-img-top" src="https://ecs7.tokopedia.net/img/cache/700/product-1/2018/2/2/150923423/150923423_688a7d67-dda4-41bd-849b-b4194f67a04a_1000_1000.png" alt="Card image cap" id="card-gambar"style="border-radius: 30px;">
+                    <div class="card-body">
+                        <h5 class="card-title" id="nama-obat">Profis Ibuprofen</h5>
+                        <p>Lorem ipsum dolor sit amet..</p>
+                        <a href="#" class="btn btn-primary">Pesan</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+	</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
