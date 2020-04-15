315c5ba3e3722717a0a0ffe9c00b0878032c34dc
figoakbar <49114403+figoakbar@users.noreply.github.com>
2020-04-15 18:47:45 +0700

    Create HomeRumahSakit.php

---
+++ application/views/HomeRumahSakit.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.0/css/mdb.min.css" rel="stylesheet">
+    <title>Admin Page</title>
+</head>
+<style type="text/css">
+        #nav-item a {
+            color: #444547;
+        }
+
+        #login {
+            margin-top: 50px;
+            width: 30%;
+            margin-bottom: 80px;
+        }
+        body{
+            padding: 0px;
+            background-image: url('https://izkey.com/wp-content/uploads/2019/03/rumah-sakit-di-jakarta-1038x576.jpg');
+            background-size:cover
+        }
+
+</style>
+<body>
+            <div class="card mx-auto" id="login">
+            <form class=" border border-light p-5">
+                <h1><img src="../assets/image/Logo.png">Imunisasi</h1><br><br>
+                <table class= "table">
+                    <thead>
+                        <tr>
+                        <th scope="col">ID Rumah Sakit</th>
+                        <th scope="col">Nama Rumah Sakit</th>
+                        <th scope="col">Delete</th>
+                        <th scope="col">Edit</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        </tr>
+                    </tbody>
+                </table>
+                <a href="<?= site_url('InputRSController')?>" class="btn btn-info btn-block my-4" type="submit" style="border-radius: 10px;">Input Rumah Sakit</a>
+                <a href="<?= site_url('HomeAdminController') ?>" type="submit" style="border-radius: 10px;"> < Back</a>
+            </form>
+        </div>
+</body>
+
+</html>
