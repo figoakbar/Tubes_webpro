0b918c9125fb5385c22dd6d62ce4f2ce5a859013
figoakbar <49114403+figoakbar@users.noreply.github.com>
2020-04-15 14:06:08 +0700

    Update HomeInputImunisasi.php

--- application/views/HomeInputImunisasi.php
+++ application/views/HomeInputImunisasi.php
@@ -37,7 +37,7 @@
                 <h1><img src="../assets/image/Logo.png">Imunisasi</h1><br><br>
                 <a href="<?= site_url('InputImunisasiController')?>" class="btn btn-info btn-block my-4" type="submit" style="border-radius: 10px;">Input Imunisasi</a>
                 <a href="<?= site_url('inputImunisasiJadwalController') ?>" class="btn btn-info btn-block my-4" type="submit" style="border-radius: 10px;">Input Jadwal</a>
-                <a href="<?= site_url('HomeAdminController') ?>" class="btn btn-info btn-block my-4" type="submit" style="border-radius: 10px;">Back</a>
+                <a href="<?= site_url('HomeAdminController') ?>" type="submit" style="border-radius: 10px;"> < Back</a>
             </form>
             <!-- Default form login -->
         </div>
