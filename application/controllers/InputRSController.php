e270f1478a5a49375dfa54a8f540f8bb94b54620
Aldy Renaldi <44864241+aldyrenaldi@users.noreply.github.com>
2020-04-20 20:14:25 +0700

    controller&imunisasi
    update inputRSController
    viewImunisasi

--- application/controllers/InputRSController.php
+++ application/controllers/InputRSController.php
@@ -17,12 +17,9 @@ class InputRSController extends CI_Controller{
 
     public function addHospitalFinal(){
         $nama = $this->input->post('namaRS');
-        $config['upload_path']          = './assets/uploads'; //isi dengan nama folder temoat menyimpan gambar
+        $config['upload_path']          = './assets/'; //isi dengan nama folder temoat menyimpan gambar
         $config['allowed_types']        =  'gif|jpg|png';//isi dengan format/tipe gambar yang diterima
-        $config['max_size']             =  549;//isi dengan ukuran maksimum yang bisa di upload
-        $config['max_width']            = '1024'; //isi dengan lebar maksimum gambar yang bisa di upload
-        $config['mac_height']           = '768';//isi dengan panjang maksimum gambar yang bisa di upload
-
+        
         $this->load->library('upload', $config);
         if (!$this->upload->do_upload('foto')){
             $config['info'] = $this->upload->display_errors();
--- application/views/imunisasi.php
+++ application/views/imunisasi.php
@@ -160,5 +160,17 @@
                     </div>
                 </footer>
     </section>
+    </script>
+    <!-- JQuery -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js">
+    </script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.7/js/mdb.min.js">
+    </script>
 </body>
 </html>
